150c48574785a94eb8bca078f93466ddf8be0380
oop778 <oskardhavel@gmail.com>
2024-11-11 13:29:23 +0200

    Allow accessing previous nodes in processors & tag itself

--- src/main/java/dev/oop778/blixx/api/tag/BlixxProcessor.java
+++ src/main/java/dev/oop778/blixx/api/tag/BlixxProcessor.java
@@ -2,17 +2,12 @@ package dev.oop778.blixx.api.tag;
 
 import dev.oop778.blixx.api.Blixx;
 import dev.oop778.blixx.api.parser.ParsingContext;
-import dev.oop778.blixx.api.parser.indexable.IndexableKey;
+import dev.oop778.blixx.api.parser.node.BlixxNode;
 import dev.oop778.blixx.util.FastComponentBuilder;
 import dev.oop778.blixx.util.ObjectArray;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NonNull;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import net.kyori.adventure.text.format.Style;
-
-import java.util.List;
 
 public interface BlixxProcessor {
     interface Tree extends BlixxProcessor {
@@ -35,11 +30,12 @@ public interface BlixxProcessor {
         @EqualsAndHashCode(callSuper = true)
         class ComponentContext extends Context {
             private Object data;
+            private BlixxTag.WithDefinedData<?> tag;
             private Style.Builder styleBuilder;
             private FastComponentBuilder componentBuilder;
 
             public <T> T getData() {
-                return (T) data;
+                return (T) this.data;
             }
         }
     }
@@ -48,13 +44,16 @@ public interface BlixxProcessor {
     @Getter
     class Context {
         private Blixx blixx;
+
+        @Setter
+        private BlixxNode node;
     }
 
     @SuperBuilder
     class ParserContext extends Context {
         private ParsingContext parsingContext;
 
-        public IndexableKey createKey() {
+        public Object createKey() {
             return this.parsingContext.createNewKey();
         }
     }
