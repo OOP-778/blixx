9998ea8cf986cba96b0ffc08cb0c03c385a82457
oop778 <oskardhavel@gmail.com>
2024-11-12 10:03:23 +0200

    Added methods to change contexts with copies

--- src/main/java/dev/oop778/blixx/api/placeholder/context/PlaceholderContext.java
+++ src/main/java/dev/oop778/blixx/api/placeholder/context/PlaceholderContext.java
@@ -1,6 +1,9 @@
 package dev.oop778.blixx.api.placeholder.context;
 
+import org.jetbrains.annotations.CheckReturnValue;
+
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface PlaceholderContext {
@@ -15,6 +18,15 @@ public interface PlaceholderContext {
     static PlaceholderContext create(Object... objects) {
         final PlaceholderContextImpl placeholderContext = new PlaceholderContextImpl();
         for (final Object object : objects) {
+            placeholderContext.register(object);
+        }
+
+        return placeholderContext;
+    }
+
+    static PlaceholderContext createWithInheritance(Object... objects) {
+        final PlaceholderContextImpl placeholderContext = new PlaceholderContextImpl();
+        for (final Object object : objects) {
             placeholderContext.registerForHierarchy(object);
         }
 
@@ -22,4 +34,16 @@ public interface PlaceholderContext {
     }
 
     <T> Optional<T> find(Class<T> clazz);
+    <T> List<T> findAll(Class<T> clazz);
+
+    @CheckReturnValue
+    PlaceholderContext withExact(Object object);
+
+    @CheckReturnValue
+    PlaceholderContext withInheritance(Object object);
+
+    @CheckReturnValue
+    default PlaceholderContext with(PlaceholderContext context) {
+        return compose(context, this);
+    }
 }
