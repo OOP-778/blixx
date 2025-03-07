8b84834be45e7c94e04dab863fb6ee9aa283cc7d
oop778 <oskardhavel@gmail.com>
2025-03-07 11:50:21 +0200

    Added all aliases of MiniMessage tags & included sc tag

--- src/main/java/dev/oop778/blixx/api/tag/BlixxTags.java
+++ src/main/java/dev/oop778/blixx/api/tag/BlixxTags.java
@@ -4,10 +4,20 @@ import dev.oop778.blixx.tag.decoration.*;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public interface BlixxTags {
+    Map<TextDecoration, List<String>> DECORATIONS = new HashMap<TextDecoration, List<String>>() {{
+        this.put(TextDecoration.BOLD, Arrays.asList("bold", "b"));
+        this.put(TextDecoration.UNDERLINED, Arrays.asList("underlined", "u"));
+        this.put(TextDecoration.STRIKETHROUGH, Arrays.asList("strikethrough", "st"));
+        this.put(TextDecoration.OBFUSCATED, Arrays.asList("obfuscated", "obf"));
+        this.put(TextDecoration.ITALIC, Arrays.asList("italic", "em", "i"));
+    }};
+
     Map<String, BlixxTag<?>> STANDARD = new HashMap<String, BlixxTag<?>>() {{
         this.put("color", ColorTag.INSTANCE);
         this.put("decorate", DecorationTag.INSTANCE);
@@ -15,14 +25,18 @@ public interface BlixxTags {
         this.put("gradient", GradientTag.INSTANCE);
         this.put("hover", HoverTag.INSTANCE);
         this.put("click", ClickTag.INSTANCE);
+        this.put("small_caps", SmallCapsTag.INSTANCE);
+        this.put("sc", SmallCapsTag.INSTANCE);
 
-        for (final TextDecoration value : TextDecoration.values()) {
-            this.put(value.name().toLowerCase(), new TagShortener<>(DecorationTag.INSTANCE, value));
+        for (final Entry<TextDecoration, List<String>> decorationEntry : DECORATIONS.entrySet()) {
+            final TagShortener<TextDecoration> tag = new TagShortener<>(DecorationTag.INSTANCE, decorationEntry.getKey());
+            for (final String identifier : decorationEntry.getValue()) {
+                this.put(identifier, tag);
+            }
         }
 
         for (final NamedTextColor namedTextColor : NamedTextColor.NAMES.values()) {
             this.put(namedTextColor.toString(), new TagShortener<>(ColorTag.INSTANCE, namedTextColor));
         }
     }};
-
 }
